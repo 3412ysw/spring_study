54e71dc710a324e162ad4a38dd4f7d60d8be6820
3412ysw <3412ysw@gmail.com>
2025-03-13 17:08:01 +0900

    [ysw] 3/13

---
+++ mvc/src/main/java/com/gn/mvc/controller/MemberController.java
@@ -0,0 +1,47 @@
+package com.gn.mvc.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gn.mvc.dto.MemberDto;
+import com.gn.mvc.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Controller
+@RequiredArgsConstructor  // final때문에 들어감 final 필드를 활용한 생성자 
+public class MemberController {
+	
+	private final MemberService service; //값이 1회성 -> 순환참조 발생 ㄴ ㄴ 
+
+	@GetMapping("/member/create")
+	public String CreateMemberViews() {
+	System.out.println("회원가입 화면전환 ");
+		return "member/create";
+	}
+	
+	@PostMapping("/member")
+	@ResponseBody //응답해주는 형식 
+	public Map<String,String> createMemberApi(MemberDto dto) {
+		Map<String,String> resultMap = new HashMap<String,String>();
+		resultMap.put("res_code", "500");
+		resultMap.put("res_msg", "회원가입 중 오류가 발생하였습니다.");
+	
+		// Service가 가지고 있는 createBoard 메소드 호출
+		MemberDto createdMember = service.createMember(dto);
+		
+		if(createdMember != null) {
+			resultMap.put("res_code", "200");
+			resultMap.put("res_msg", "회원가입 됨 ㄹㅇㅋㅋ");
+		}
+		
+		return resultMap;
+	}
+
+}
